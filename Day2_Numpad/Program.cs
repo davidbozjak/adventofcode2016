7bd939d5e697524d03555bb63a2f558ae872cc34
David Božjak <bozjak@gmail.com>
2022-04-10 22:17:15 +0200

    Added Part 2

--- Day2_Numpad/Program.cs
+++ Day2_Numpad/Program.cs
@@ -4,42 +4,54 @@ var instructions = new InputProvider<Instruction?>("Input.txt", GetInstructions)
     .Where(w => w != null).Cast<Instruction>()
     .ToList();
 
-Dictionary<(int x, int y), char> keyPad = new()
+Dictionary<(int x, int y), char> part1KeyPad = new()
 {
-    { (0, 0), '1' },
-    { (1, 0), '2' },
-    { (2, 0), '3' },
-    { (0, 1), '4' },
-    { (1, 1), '5' },
-    { (2, 1), '6' },
-    { (0, 2), '7' },
-    { (1, 2), '8' },
-    { (2, 2), '9' },
+    { (0, 0), '1' }, { (1, 0), '2' }, { (2, 0), '3' },
+    { (0, 1), '4' }, { (1, 1), '5' }, { (2, 1), '6' },
+    { (0, 2), '7' }, { (1, 2), '8' }, { (2, 2), '9' },
 };
 
-int x = 1, y = 1;
-StringBuilder codeWIP = new();
-
-foreach (var instruction in instructions)
+Dictionary<(int x, int y), char> part2KeyPad = new()
 {
-    foreach (var step in instruction.Steps)
+    { (2, 0), '1' },
+    { (1, 1), '2' }, { (2, 1), '3' }, { (3, 1), '4' },
+    { (0, 2), '5' }, { (1, 2), '6' }, { (2, 2), '7' }, { (3, 2), '8' }, { (4, 2), '9' },
+    { (1, 3), 'A' }, { (2, 3), 'B' }, { (3, 3), 'C' },
+    { (2, 4), 'D' }
+};
+
+
+Console.WriteLine($"Part 1: {FollowInstructions((1, 1), instructions, part1KeyPad)}");
+Console.WriteLine($"Part 2: {FollowInstructions((0, 2), instructions, part2KeyPad)}");
+
+static string FollowInstructions((int x, int y) pos, IEnumerable<Instruction> instructions, Dictionary<(int x, int y), char> keyPad)
+{
+    StringBuilder codeWIP = new();
+
+    foreach (var instruction in instructions)
     {
-        (x, y) = step switch
+        foreach (var step in instruction.Steps)
         {
-            Direction.Up => (x, y - 1),
-            Direction.Down => (x, y + 1),
-            Direction.Left => (x - 1, y),
-            Direction.Right => (x + 1, y),
-            _ => throw new Exception()
-        };
-        x = Math.Max(0, Math.Min(2, x));
-        y = Math.Max(0, Math.Min(2, y));
+            var newPos = step switch
+            {
+                Direction.Up => (pos.x, pos.y - 1),
+                Direction.Down => (pos.x, pos.y + 1),
+                Direction.Left => (pos.x - 1, pos.y),
+                Direction.Right => (pos.x + 1, pos.y),
+                _ => throw new Exception()
+            };
+            
+            if (keyPad.ContainsKey(newPos))
+            {
+                pos = newPos;
+            }
+        }
+
+        codeWIP.Append(keyPad[pos]);
     }
 
-    codeWIP.Append(keyPad[(x, y)]);
+    return codeWIP.ToString();
 }
-
-Console.WriteLine($"Part 1: {codeWIP}");
 
 static bool GetInstructions(string? input, out Instruction? value)
 {
