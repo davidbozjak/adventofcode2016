f86d0604d4b5f5636fa6d0a8085e66052285a8e8
David Božjak <bozjak@gmail.com>
2022-04-13 22:14:50 +0200

    Added day 6

---
+++ Day6_Repetition/Program.cs
@@ -0,0 +1,33 @@
+﻿using System.Text;
+
+var messages = new InputProvider<string?>("Input.txt", GetString).Where(w => w != null).Cast<string>().ToList();
+
+var messageLength = messages[0].Length;
+var mostFrequentKeyMessageBuilder = new StringBuilder();
+var leastFrequentKeyMessageBuilder = new StringBuilder();
+
+for (int i = 0; i < messageLength; i++)
+{
+    var letters = messages.Select(w => w[i]);
+    var frequencies = letters.GroupBy(w => w).ToDictionary(w => w.Key, w => w.Count());
+    
+    var mostFrequent = frequencies.OrderByDescending(w => w.Value).Select(w => w.Key).First();
+    mostFrequentKeyMessageBuilder.Append(mostFrequent);
+    
+    var leastFrequent = frequencies.OrderBy(w => w.Value).Select(w => w.Key).First();
+    leastFrequentKeyMessageBuilder.Append(leastFrequent);
+}
+
+Console.WriteLine($"Part 1: {mostFrequentKeyMessageBuilder}");
+Console.WriteLine($"Part 2: {leastFrequentKeyMessageBuilder}");
+
+static bool GetString(string? input, out string? value)
+{
+    value = null;
+
+    if (input == null) return false;
+
+    value = input ?? string.Empty;
+
+    return true;
+}
