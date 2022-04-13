92a9e74f621a20cc854deb1cee3af39b5b9788c1
David Božjak <bozjak@gmail.com>
2022-04-13 21:54:15 +0200

    Adding day 5 part 1

---
+++ Day5_MD5/Program.cs
@@ -0,0 +1,23 @@
+﻿using System.Text;
+
+string doorID = "ojvtpuvg";
+
+var passwordBuilder = new StringBuilder();
+
+using (var md5 = System.Security.Cryptography.MD5.Create())
+{
+    for (long i = 0; passwordBuilder.Length < 8; i++)
+    {
+        var input = Encoding.ASCII.GetBytes(doorID + i.ToString());
+        var hashbytes = md5.ComputeHash(input);
+
+        var hexRepresentation = Convert.ToHexString(hashbytes);
+
+        if (hexRepresentation[..5] == "00000")
+        {
+            passwordBuilder.Append(hexRepresentation[5]);
+        }
+    }
+}
+
+Console.WriteLine($"Part 1: {passwordBuilder}");
