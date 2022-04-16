713a94c546667e0833bdbb371daf3002f1ccf9da
David Božjak <bozjak@gmail.com>
2022-04-16 21:35:46 +0200

    Added Day 9 part 1

---
+++ Day9_DeCompress/Program.cs
@@ -0,0 +1,50 @@
+﻿using System.Text;
+
+string compressedString = new InputProvider<string?>("Input.txt", GetString).First();
+
+StringBuilder outputBuilder = new StringBuilder();
+
+for (int i = 0; i < compressedString.Length; i++)
+{
+    if (compressedString[i] == '(')
+    {
+        string noCharsString = string.Empty;
+        for (i++; compressedString[i] != 'x'; i++)
+        {
+            noCharsString += compressedString[i];
+        }
+
+        string timesString = string.Empty;
+        for (i++; compressedString[i] != ')'; i++)
+        {
+            timesString += compressedString[i];
+        }
+
+        int noChars = int.Parse(noCharsString);
+        string strToRepeat = compressedString.Substring(i + 1, noChars);
+        i += noChars;
+
+        for (int times = int.Parse(timesString); times > 0; times--)
+        {
+            outputBuilder.Append(strToRepeat);
+        }
+    }
+    else
+    { 
+        outputBuilder.Append(compressedString[i]); 
+    }
+}
+
+Console.WriteLine($"Part 1: {outputBuilder.Length}");
+Console.WriteLine(outputBuilder);
+
+static bool GetString(string? input, out string? value)
+{
+    value = null;
+
+    if (input == null) return false;
+
+    value = input ?? string.Empty;
+
+    return true;
+}
