b7473e88e33a56694763674db155de661e286b05
David Božjak <bozjak@gmail.com>
2022-04-25 20:27:05 +0200

    Added Day 12

---
+++ Day12_Monorail/Program.cs
@@ -0,0 +1,23 @@
+﻿var instructionStrings = new InputProvider<string?>("Input.txt", GetString).Cast<string>().ToList();
+
+var computer = new Computer(instructionStrings);
+computer.Run();
+
+Console.WriteLine($"Part 1: {computer.GetRegisterValue("a")}");
+
+computer = new Computer(instructionStrings);
+computer.SetRegisterValue("c", 1);
+computer.Run();
+
+Console.WriteLine($"Part 2: {computer.GetRegisterValue("a")}");
+
+static bool GetString(string? input, out string? value)
+{
+    value = null;
+
+    if (input == null) return false;
+
+    value = input ?? string.Empty;
+
+    return true;
+}
