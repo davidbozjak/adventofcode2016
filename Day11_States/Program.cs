4bab7d59fab9a5e48bdadcd2b1dcfe2949083952
David Božjak <bozjak@gmail.com>
2022-04-25 07:00:37 +0200

    Added state validation

--- Day11_States/Program.cs
+++ Day11_States/Program.cs
@@ -9,6 +9,12 @@ var lm = new Item("LM");
 var hg = new Item("HG");
 var hm = new Item("HM");
 
+lm.SetForbiddenItemList(new[] { hg });
+lm.ShieldItem = lg;
+
+hm.SetForbiddenItemList(new[] { lg });
+hm.ShieldItem = hg;
+
 //Instructions:
 //The first floor contains a hydrogen-compatible microchip and a lithium-compatible microchip.
 //The second floor contains a hydrogen generator.
@@ -30,13 +36,22 @@ var desiredState = new State(new[]
         new [] { elevator, hg, hm, lm, lg }
     });
 
+if (!IsStateValid(initialState)) throw new Exception();
+if (!IsStateValid(desiredState)) throw new Exception();
+
 var path = AStarPathfinder.FindPath(initialState, desiredState, w => 0, GetValidMoves);
 
-Console.WriteLine($"Part 1: {path.Count}");
+Console.WriteLine($"Part 1: {path.Count - 1} steps");
 
 IEnumerable<State> GetValidMoves(State state)
 {
-    return GetAllMoves(state).Where(IsStateValid);
+    var allMoves = GetAllMoves(state);
+
+    var validMoves = allMoves.Where(IsStateValid).ToList();
+
+    return validMoves;
+
+    //return GetAllMoves(state).Where(IsStateValid);
 }
 
 IEnumerable<State> GetAllMoves(State state)
@@ -61,24 +76,32 @@ IEnumerable<State> GetAllMoves(State state)
     //generate possible moves where items go up if not on top
     if (elevatorFloor < 3)
     {
-        //elevator moves up alone
-        yield return MoveItemsFromFloorToFloor(elevatorFloor + 1, state, new[] { elevator });
-
-        foreach (var permutation in floorWithoutElevator.PermuteList())
+        foreach (var item in floorWithoutElevator)
         {
-            yield return MoveItemsFromFloorToFloor(elevatorFloor + 1, state, permutation.Append(elevator).ToList());
+            yield return MoveItemsFromFloorToFloor(elevatorFloor + 1, state, new[] { elevator, item });
+
+            foreach (var secondItem in floorWithoutElevator)
+            {
+                if (item == secondItem) continue;
+
+                yield return MoveItemsFromFloorToFloor(elevatorFloor + 1, state, new[] { elevator, item, secondItem });
+            }
         }
     }
 
     //generate possible moves where items go down if not at bottom
-    if (elevatorFloor > 1)
+    if (elevatorFloor > 0)
     {
-        //elevator moves down alone
-        yield return MoveItemsFromFloorToFloor(elevatorFloor - 1, state, new[] { elevator });
-
-        foreach (var permutation in floorWithoutElevator.PermuteList())
+        foreach (var item in floorWithoutElevator)
         {
-            yield return MoveItemsFromFloorToFloor(elevatorFloor - 1, state, permutation.Append(elevator));
+            yield return MoveItemsFromFloorToFloor(elevatorFloor - 1, state, new[] { elevator, item });
+
+            foreach (var secondItem in floorWithoutElevator)
+            {
+                if (item == secondItem) continue;
+
+                yield return MoveItemsFromFloorToFloor(elevatorFloor - 1, state, new[] { elevator, item, secondItem });
+            }
         }
     }
 }
@@ -116,7 +139,29 @@ State MoveItemsFromFloorToFloor(int newFloor, State originalState, IEnumerable<I
 
 static bool IsStateValid(State state)
 {
+    foreach (var floor in state.Floors)
+    {
+        if (!IsFloorValid(floor))
+            return false;
+    }
+
     return true;
+
+    bool IsFloorValid(IEnumerable<Item> floor)
+    {
+        foreach (var item in floor)
+        {
+            // In other words, if a chip is ever left in the same area as another RTG,
+            // and it's not connected to its own RTG, the chip will be fried.
+            if (floor.Any(w => item.ForbiddenItemList.Contains(w)))
+            {
+                if (!floor.Contains(item.ShieldItem))
+                    return false;
+            }
+        }
+
+        return true;
+    }
 }
 
 [DebuggerDisplay("{Name}")]
@@ -125,11 +170,21 @@ class Item
     public string Name { get; }
 
     private readonly List<Item> forbiddenItemList = new();
-    private readonly List<Item> shildedItemList = new();
+
+    public IReadOnlyCollection<Item> ForbiddenItemList => this.forbiddenItemList.AsReadOnly();
+    public Item ShieldItem { get; set; }
+
 
     public Item(string name)
     {
         this.Name = name;
+    }
+
+    public void SetForbiddenItemList(IEnumerable<Item> items)
+    {
+        if (this.forbiddenItemList.Count != 0) throw new Exception();
+
+        this.forbiddenItemList.AddRange(items);
     }
 }
 
