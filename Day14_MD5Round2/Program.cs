dc2ee4afa89de083ffaf39c62d86829305de3676
David Božjak <bozjak@gmail.com>
2022-04-25 23:06:37 +0200

    Added Day 14 part 1

---
+++ Day14_MD5Round2/Program.cs
@@ -0,0 +1,69 @@
+﻿using System.Text;
+
+//string salt = "abc"; // debug string
+string salt = "ngcjuoqr";
+
+var generatedHashes = new List<string>();
+var keys = new List<int>();
+
+for (int index = 0; keys.Count < 64; index++)
+{
+    var c = FindFirstTripletInString(GetOrCreateHashN(index));
+
+    if (c != null)
+    {
+        var strToConfirmKey = new string(Enumerable.Repeat((char)c, 5).ToArray());
+        for (int i = 1; i <= 1000; i++)
+        {
+            var hash = GetOrCreateHashN(index + i);
+
+            if (hash.Contains(strToConfirmKey))
+            {
+                keys.Add(index);
+                break;
+            }
+        }
+    }
+}
+
+Console.WriteLine($"Part 1: {keys[63]}");
+
+string GetOrCreateHashN(int n)
+{
+    if (generatedHashes.Count <= n)
+    {
+        using (var md5 = System.Security.Cryptography.MD5.Create())
+        {
+            var existing = generatedHashes.Count;
+            for (int i = 0; i < 2000; i++)
+            {
+                var input = Encoding.ASCII.GetBytes(salt + (existing + i).ToString());
+                var hashbytes = md5.ComputeHash(input);
+
+                var hexRepresentation = Convert.ToHexString(hashbytes);
+                generatedHashes.Add(hexRepresentation);
+            }
+        }
+    }
+
+    return generatedHashes[n];
+}
+
+char? FindFirstTripletInString(string input)
+{
+    int repeated = 1;
+
+    for (int i = 1; i < input.Length; i++)
+    {
+        if (input[i] == input[i - 1])
+            repeated++;
+        else repeated = 1;
+
+        if (repeated == 3)
+        {
+            return input[i];
+        }
+    }
+
+    return null;
+}
