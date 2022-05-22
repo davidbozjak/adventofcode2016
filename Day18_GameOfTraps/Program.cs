ac3e04f0bceabd0185a4d85f6105a1a20190be6d
David Božjak <bozjak@gmail.com>
2022-05-22 13:39:18 +0200

    Refactored to a class

--- Day18_GameOfTraps/Program.cs
+++ Day18_GameOfTraps/Program.cs
@@ -1,13 +1,8 @@
 ﻿using System.Drawing;
-using System.Text.RegularExpressions;
-
-Regex numRegex = new(@"\d+");
-Regex hexColorRegex = new(@"#[0-9a-z][0-9a-z][0-9a-z][0-9a-z][0-9a-z][0-9a-z]");
 
 var initialTilesParser = new SingleLineStringInputParser<ITile>(GetSetStateTileFromChar, str => str.ToCharArray().Select(w => w.ToString()).ToArray());
 var initialTiles = new InputProvider<ITile>("Input.txt", initialTilesParser.GetValue).ToList();
 var width = initialTiles.Count;
-
 
 for (int x = 0; x < width; x++)
 {
@@ -15,32 +10,8 @@ for (int x = 0; x < width; x++)
     tile.Position = new Point(x, 0);
 }
 
-List<ITile> lastTileRow = new List<ITile>(initialTiles);
-int totalSafeCells = lastTileRow.Count(w => !w.IsTrap);
-
-for (int row = 1; row < 400000; row++)
-{
-    var newRow = new List<ITile>(lastTileRow.Count);
-
-    for (int x = 0; x < width; x++)
-    {
-        var tile = new CalculatedStateTile()
-        {
-            Position = new Point(x, row),
-            Left = x > 0 ? lastTileRow[x - 1] : null,
-            Center = lastTileRow[x],
-            Right = x < width - 1 ? lastTileRow[x + 1] : null,
-
-        };
-
-        newRow.Add(tile);
-    }
-
-    lastTileRow = newRow;
-    totalSafeCells += lastTileRow.Count(w => !w.IsTrap);
-}
-
-Console.WriteLine(totalSafeCells);
+Console.WriteLine($"Part 1: {GenerateTileGridFromInitialTiles(initialTiles, 40)}");
+Console.WriteLine($"Part 2: {GenerateTileGridFromInitialTiles(initialTiles, 400000)}");
 
 static bool GetSetStateTileFromChar(string? input, out ITile? value)
 {
@@ -57,76 +28,34 @@ static bool GetSetStateTileFromChar(string? input, out ITile? value)
     return true;
 }
 
-interface ITile : IWorldObject
+static int GenerateTileGridFromInitialTiles(List<ITile> initialTiles, int rowsToGenerate)
 {
-    bool IsTrap { get; }
-}
+    int width = initialTiles.Count;
+    List<ITile> lastTileRow = new List<ITile>(initialTiles);
+    int totalSafeCells = lastTileRow.Count(w => !w.IsTrap);
 
-class SetStateTile : ITile
-{
-    public bool IsTrap { get; init; }
-
-    public Point Position { get; set; }
-
-    public char CharRepresentation => this.IsTrap ? '^' : '.';
-
-    public int Z => 0;
-}
-
-class CalculatedStateTile : ITile
-{
-    private Cached<bool> cachedIsTrap;
-    public bool IsTrap => this.cachedIsTrap.Value;
-
-    public Point Position { get; init; }
-
-    public char CharRepresentation => this.cachedIsTrap.Value ? '^' : '.';
-
-    public int Z => 0;
-
-    public ITile? Left { get; set; }
-     
-    public ITile? Center { get; set; }
-     
-    public ITile? Right { get; set; }
-
-    public CalculatedStateTile()
+    for (int row = 1; row < rowsToGenerate; row++)
     {
-        cachedIsTrap = new Cached<bool>(GetIsTrapAndCleanUp);
-    }
+        var newRow = new List<ITile>(lastTileRow.Count);
 
-    private bool GetIsTrapAndCleanUp()
-    {
-        bool isTrap = GetIsTrap();
-
-        this.Left = null;
-        this.Center = null;
-        this.Right = null;
-
-        return isTrap;
-
-        bool GetIsTrap()
+        for (int x = 0; x < width; x++)
         {
-            if (this.cachedIsTrap.IsValueCreated)
-                throw new Exception();
+            var tile = new CalculatedStateTile()
+            {
+                Position = new Point(x, row),
+                Left = x > 0 ? lastTileRow[x - 1] : null,
+                Center = lastTileRow[x],
+                Right = x < width - 1 ? lastTileRow[x + 1] : null,
 
-            //left and center but not right
-            if ((this.Left?.IsTrap ?? false) && (this.Center?.IsTrap ?? false) && !(this.Right?.IsTrap ?? false))
-                return true;
+            };
 
-            //center and right but not left
-            if (!(this.Left?.IsTrap ?? false) && (this.Center?.IsTrap ?? false) && (this.Right?.IsTrap ?? false))
-                return true;
-
-            // only left
-            if ((this.Left?.IsTrap ?? false) && !(this.Center?.IsTrap ?? false) && !(this.Right?.IsTrap ?? false))
-                return true;
-
-            // only right
-            if (!(this.Left?.IsTrap ?? false) && !(this.Center?.IsTrap ?? false) && (this.Right?.IsTrap ?? false))
-                return true;
-
-            return false;
+            newRow.Add(tile);
         }
+
+        lastTileRow = newRow;
+        totalSafeCells += lastTileRow.Count(w => !w.IsTrap);
     }
+
+    return totalSafeCells;
 }
+
---
+++ Day18_GameOfTraps/Tile.cs
@@ -0,0 +1,75 @@
+﻿using System.Drawing;
+
+interface ITile : IWorldObject
+{
+    bool IsTrap { get; }
+}
+
+class SetStateTile : ITile
+{
+    public bool IsTrap { get; init; }
+
+    public Point Position { get; set; }
+
+    public char CharRepresentation => this.IsTrap ? '^' : '.';
+
+    public int Z => 0;
+}
+
+class CalculatedStateTile : ITile
+{
+    private Cached<bool> cachedIsTrap;
+    public bool IsTrap => this.cachedIsTrap.Value;
+
+    public Point Position { get; init; }
+
+    public char CharRepresentation => this.cachedIsTrap.Value ? '^' : '.';
+
+    public int Z => 0;
+
+    public ITile? Left { get; set; }
+
+    public ITile? Center { get; set; }
+
+    public ITile? Right { get; set; }
+
+    public CalculatedStateTile()
+    {
+        cachedIsTrap = new Cached<bool>(GetIsTrapAndCleanUp);
+    }
+
+    private bool GetIsTrapAndCleanUp()
+    {
+        bool isTrap = GetIsTrap();
+
+        this.Left = null;
+        this.Center = null;
+        this.Right = null;
+
+        return isTrap;
+
+        bool GetIsTrap()
+        {
+            if (this.cachedIsTrap.IsValueCreated)
+                throw new Exception();
+
+            //left and center but not right
+            if ((this.Left?.IsTrap ?? false) && (this.Center?.IsTrap ?? false) && !(this.Right?.IsTrap ?? false))
+                return true;
+
+            //center and right but not left
+            if (!(this.Left?.IsTrap ?? false) && (this.Center?.IsTrap ?? false) && (this.Right?.IsTrap ?? false))
+                return true;
+
+            // only left
+            if ((this.Left?.IsTrap ?? false) && !(this.Center?.IsTrap ?? false) && !(this.Right?.IsTrap ?? false))
+                return true;
+
+            // only right
+            if (!(this.Left?.IsTrap ?? false) && !(this.Center?.IsTrap ?? false) && (this.Right?.IsTrap ?? false))
+                return true;
+
+            return false;
+        }
+    }
+}
