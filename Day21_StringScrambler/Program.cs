90e5a4265c54f46358a9b6e87367610700cf7895
David Božjak <bozjak@gmail.com>
2022-06-04 09:13:00 +0200

    Added parsing

--- Day21_StringScrambler/Program.cs
+++ Day21_StringScrambler/Program.cs
@@ -1,19 +1,21 @@
 ﻿using System.Text;
+using System.Text.RegularExpressions;
 
-//var wholeStringInput = new InputProvider<string>("Input.txt", GetString).ToList();
 
 var input = "abcde";
 
-var instructionList = new List<IInstruction>();
+//var instructionList = new List<IInstruction>();
 
-instructionList.Add(new SwapPositionInstruction(4, 0));
-instructionList.Add(new SwapLetterInstruction('d', 'b'));
-instructionList.Add(new ReversePositionsInstruction(0, 4));
-instructionList.Add(new RotateInstruction(-1));
-instructionList.Add(new MovePositionsInstruction(1, 4));
-instructionList.Add(new MovePositionsInstruction(3, 0));
-instructionList.Add(new RotateRightBasedOnPositionInstruction('b'));
-instructionList.Add(new RotateRightBasedOnPositionInstruction('d'));
+//instructionList.Add(new SwapPositionInstruction(4, 0));
+//instructionList.Add(new SwapLetterInstruction('d', 'b'));
+//instructionList.Add(new ReversePositionsInstruction(0, 4));
+//instructionList.Add(new RotateInstruction(-1));
+//instructionList.Add(new MovePositionsInstruction(1, 4));
+//instructionList.Add(new MovePositionsInstruction(3, 0));
+//instructionList.Add(new RotateRightBasedOnPositionInstruction('b'));
+//instructionList.Add(new RotateRightBasedOnPositionInstruction('d'));
+
+var instructionList = new InputProvider<IInstruction?>("Input.txt", ParseInstructionFromInput).Where(w => w != null).Cast<IInstruction>().ToList();
 
 var scrambledString = input;
 
@@ -26,13 +28,43 @@ foreach (var instruction in instructionList)
 
 Console.WriteLine($"Part 1: {scrambledString}");
 
-static bool GetString(string? input, out string? value)
+static bool ParseInstructionFromInput(string? input, out IInstruction? value)
 {
     value = null;
 
     if (input == null) return false;
 
-    value = input ?? string.Empty;
+    Regex numRegex = new(@"\d+");
+    var numbers = numRegex.Matches(input).Select(w => int.Parse(w.Value)).ToArray();
+
+    if (input.StartsWith("swap position"))
+    {
+        value = new SwapPositionInstruction(numbers[0], numbers[1]);
+    }
+    else if (input.StartsWith("swap letter"))
+    {
+        value = new SwapLetterInstruction(input[12], input[26]);
+    }
+    else if (input.StartsWith("rotate based on position of letter"))
+    {
+        value = new RotateRightBasedOnPositionInstruction(input[35]);
+    }
+    else if (input.StartsWith("rotate right"))
+    {
+        value = new RotateInstruction(numbers[0]);
+    }
+    else if (input.StartsWith("rotate left"))
+    {
+        value = new RotateInstruction(-numbers[0]);
+    }
+    else if (input.StartsWith("reverse positions"))
+    {
+        value = new ReversePositionsInstruction(numbers[0], numbers[1]);
+    }
+    else if (input.StartsWith("move position"))
+    {
+        value = new MovePositionsInstruction(numbers[0], numbers[1]);
+    }
 
     return true;
 }
