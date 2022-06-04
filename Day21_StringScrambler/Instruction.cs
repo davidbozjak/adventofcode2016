a2815856724b7c1917b878ae194e07296bf206b7
David Božjak <bozjak@gmail.com>
2022-06-04 10:12:45 +0200

    Added part 2

---
+++ Day21_StringScrambler/Instruction.cs
@@ -0,0 +1,197 @@
+﻿using System.Text;
+
+interface IInstruction
+{
+    string Process(string input);
+
+    string ProcessReversed(string input);
+}
+
+class SwapPositionInstruction : IInstruction
+{
+    private readonly int firstIndex;
+    private readonly int secondIndex;
+
+    public SwapPositionInstruction(int firstIndex, int secondIndex)
+    {
+        if (firstIndex == -1) throw new Exception();
+        if (secondIndex == -1) throw new Exception();
+
+        this.firstIndex = firstIndex;
+        this.secondIndex = secondIndex;
+    }
+
+    public string Process(string input)
+    {
+        var builder = new StringBuilder(input);
+        builder[firstIndex] = input[secondIndex];
+        builder[secondIndex] = input[firstIndex];
+        return builder.ToString();
+    }
+
+    public string ProcessReversed(string input) => Process(input);
+}
+
+class SwapLetterInstruction : IInstruction
+{
+    private readonly char firstLetter;
+    private readonly char secondLetter;
+
+    public SwapLetterInstruction(char firstLetter, char secondLetter)
+    {
+        this.firstLetter = firstLetter;
+        this.secondLetter = secondLetter;
+    }
+
+    public string Process(string input)
+    {
+        var firstIndex = input.IndexOf(firstLetter);
+        var secondIndex = input.IndexOf(secondLetter);
+
+        var swapPosition = new SwapPositionInstruction(firstIndex, secondIndex);
+        return swapPosition.Process(input);
+    }
+
+    public string ProcessReversed(string input) => Process(input);
+}
+
+class RotateInstruction : IInstruction
+{
+    private readonly int numberOfSteps;
+    public RotateInstruction(int steps)
+    {
+        this.numberOfSteps = steps;
+    }
+
+    public string Process(string input)
+    {
+        var array = new char[input.Length];
+
+        for (int i = 0; i < input.Length; i++)
+        {
+            array[wrapIndex(i + numberOfSteps)] = input[i];
+        }
+
+        return new string(array);
+
+        int wrapIndex(int index)
+        {
+            while (index >= input.Length) index -= input.Length;
+            while (index < 0) index += input.Length;
+
+            return index;
+        }
+    }
+
+    public string ProcessReversed(string input)
+    {
+        var reverseRotation = new RotateInstruction(-numberOfSteps);
+        return reverseRotation.Process(input);
+    }
+}
+
+class RotateRightBasedOnPositionInstruction : IInstruction
+{
+    private readonly char letter;
+
+    public RotateRightBasedOnPositionInstruction(char letter)
+    {
+        this.letter = letter;
+    }
+
+    public string Process(string input)
+    {
+        int indexOfLetter = input.IndexOf(letter);
+        if (indexOfLetter == -1) throw new Exception();
+
+        int steps = 1 + indexOfLetter + (indexOfLetter >= 4 ? 1 : 0);
+
+        var rotate = new RotateInstruction(steps);
+        return rotate.Process(input);
+    }
+
+    public string ProcessReversed(string input)
+    {
+        int indexOfLetter = input.IndexOf(letter);
+        if (indexOfLetter == -1) throw new Exception();
+
+        if (input.Length != 8) throw new Exception("The hack only works for input strings with a length of 8");
+
+        int steps = indexOfLetter switch
+        {
+            0 => -1,
+            1 => -1,
+            2 => -6,
+            3 => -2,
+            4 => -7,
+            5 => -3,
+            6 => 0,
+            7 => -4,
+            _ => throw new Exception()
+        };
+
+        var rotate = new RotateInstruction(steps);
+        return rotate.Process(input);
+    }
+}
+
+class ReversePositionsInstruction : IInstruction
+{
+    private readonly int lowIndex;
+    private readonly int highIndex;
+
+    public ReversePositionsInstruction(int firstIndex, int secondIndex)
+    {
+        if (firstIndex == -1) throw new Exception();
+        if (secondIndex == -1) throw new Exception();
+
+        this.lowIndex = Math.Min(firstIndex, secondIndex);
+        this.highIndex = Math.Max(firstIndex, secondIndex);
+    }
+
+    public string Process(string input)
+    {
+        var builder = new StringBuilder(input);
+
+        for (int i = this.lowIndex, j = this.highIndex; i <= this.highIndex; i++, j--)
+        {
+            builder[i] = input[j];
+        }
+
+        return builder.ToString();
+    }
+
+    public string ProcessReversed(string input) => Process(input);
+}
+
+class MovePositionsInstruction : IInstruction
+{
+    private readonly int firstIndex;
+    private readonly int secondIndex;
+
+    public MovePositionsInstruction(int firstIndex, int secondIndex)
+    {
+        if (firstIndex == -1) throw new Exception();
+        if (secondIndex == -1) throw new Exception();
+
+        this.firstIndex = firstIndex;
+        this.secondIndex = secondIndex;
+    }
+
+    public string Process(string input)
+    {
+        var list = input.ToList();
+
+        var letter = input[firstIndex];
+        list.Remove(letter);
+        list.Insert(secondIndex, letter);
+
+        return string.Join("", list);
+    }
+
+    public string ProcessReversed(string input)
+    {
+        var reverseMove = new MovePositionsInstruction(this.secondIndex, this.firstIndex);
+        return reverseMove.Process(input);
+    }
+}
--- Day21_StringScrambler/Program.cs
+++ Day21_StringScrambler/Program.cs
@@ -1,23 +1,33 @@
-﻿using System.Text;
-using System.Text.RegularExpressions;
+﻿using System.Text.RegularExpressions;
 
-
-var input = "abcde";
-
-//var instructionList = new List<IInstruction>();
-
-//instructionList.Add(new SwapPositionInstruction(4, 0));
-//instructionList.Add(new SwapLetterInstruction('d', 'b'));
-//instructionList.Add(new ReversePositionsInstruction(0, 4));
-//instructionList.Add(new RotateInstruction(-1));
-//instructionList.Add(new MovePositionsInstruction(1, 4));
-//instructionList.Add(new MovePositionsInstruction(3, 0));
-//instructionList.Add(new RotateRightBasedOnPositionInstruction('b'));
-//instructionList.Add(new RotateRightBasedOnPositionInstruction('d'));
+var input = "abcdefgh";
+//var input = "abcde";
 
 var instructionList = new InputProvider<IInstruction?>("Input.txt", ParseInstructionFromInput).Where(w => w != null).Cast<IInstruction>().ToList();
 
 var scrambledString = input;
+
+//var inputForTestingOfRotateBasedOnLetter = "abcdefgh";
+
+//foreach (char letter in inputForTestingOfRotateBasedOnLetter)
+//{
+//    var instruction = new RotateRightBasedOnPositionInstruction(letter);
+
+//    var initial = inputForTestingOfRotateBasedOnLetter;
+//    var scrambled = instruction.Process(initial);
+//    var reversed = instruction.ProcessReversed(scrambled);
+
+//    Console.WriteLine($"{letter}: {initial} becomes {scrambled} reverse becomes {reversed} - {(reversed == initial ? "correct" : "INCORRECT")}");
+//}
+
+//foreach (var instruction in instructionList)
+//{
+//    var initialString = scrambledString;
+//    scrambledString = instruction.Process(scrambledString);
+//    scrambledString = instruction.ProcessReversed(scrambledString);
+
+//    if (scrambledString != initialString) throw new Exception();
+//}
 
 foreach (var instruction in instructionList)
 {
@@ -27,6 +37,18 @@ foreach (var instruction in instructionList)
 }
 
 Console.WriteLine($"Part 1: {scrambledString}");
+
+scrambledString = "fbgdceah";
+instructionList.Reverse();
+
+foreach (var instruction in instructionList)
+{
+    Console.Write($"{scrambledString} becomes ");
+    scrambledString = instruction.ProcessReversed(scrambledString);
+    Console.WriteLine(scrambledString);
+}
+
+Console.WriteLine($"Part 2: {scrambledString}");
 
 static bool ParseInstructionFromInput(string? input, out IInstruction? value)
 {
@@ -65,158 +87,8 @@ static bool ParseInstructionFromInput(string? input, out IInstruction? value)
     {
         value = new MovePositionsInstruction(numbers[0], numbers[1]);
     }
+    else throw new Exception("Unknown instruction");
 
     return true;
 }
 
-interface IInstruction
-{
-    string Process(string input);
-}
-
-class SwapPositionInstruction : IInstruction
-{
-    private readonly int firstIndex;
-    private readonly int secondIndex;
-
-    public SwapPositionInstruction(int firstIndex, int secondIndex)
-    {
-        if (firstIndex == -1) throw new Exception();
-        if (secondIndex == -1) throw new Exception();
-
-        this.firstIndex = firstIndex;
-        this.secondIndex = secondIndex;
-    }
-
-    public string Process(string input)
-    {
-        var builder = new StringBuilder(input);
-        builder[firstIndex] = input[secondIndex];
-        builder[secondIndex] = input[firstIndex];
-        return builder.ToString();
-    }
-}
-
-class SwapLetterInstruction : IInstruction
-{
-    private readonly char firstLetter;
-    private readonly char secondLetter;
-
-    public SwapLetterInstruction(char firstLetter, char secondLetter)
-    {
-        this.firstLetter = firstLetter;
-        this.secondLetter = secondLetter;
-    }
-
-    public string Process(string input)
-    {
-        var firstIndex = input.IndexOf(firstLetter);
-        var secondIndex = input.IndexOf(secondLetter);
-
-        var swapPosition = new SwapPositionInstruction(firstIndex, secondIndex);
-        return swapPosition.Process(input);
-    }
-}
-
-class RotateInstruction : IInstruction
-{
-    private readonly int numberOfSteps;
-    public RotateInstruction(int steps)
-    {
-        this.numberOfSteps = steps;
-    }
-
-    public string Process(string input)
-    {
-        var array = new char[input.Length];
-
-        for(int i = 0; i < input.Length; i++)
-        {
-            array[wrapIndex(i + numberOfSteps)] = input[i];
-        }
-
-        return new string(array);
-
-        int wrapIndex(int index)
-        {
-            while (index >= input.Length) index -= input.Length;
-            while (index < 0) index += input.Length;
-
-            return index;
-        }
-    }
-}
-
-class RotateRightBasedOnPositionInstruction : IInstruction
-{
-    private readonly char letter;
-
-    public RotateRightBasedOnPositionInstruction(char letter)
-    {
-        this.letter = letter;
-    }
-
-    public string Process(string input)
-    {
-        int indexOfLetter = input.IndexOf(letter);
-        if (indexOfLetter == -1) throw new Exception();
-
-        int steps = 1 + indexOfLetter + (indexOfLetter >= 4 ? 1 : 0);
-        
-        var rotate = new RotateInstruction(steps);
-        return rotate.Process(input);
-    }
-}
-
-class ReversePositionsInstruction : IInstruction
-{
-    private readonly int lowIndex;
-    private readonly int highIndex;
-
-    public ReversePositionsInstruction(int firstIndex, int secondIndex)
-    {
-        if (firstIndex == -1) throw new Exception();
-        if (secondIndex == -1) throw new Exception();
-
-        this.lowIndex = Math.Min(firstIndex, secondIndex);
-        this.highIndex = Math.Max(firstIndex, secondIndex);
-    }
-
-    public string Process(string input)
-    {
-        var builder = new StringBuilder(input);
-
-        for (int i = this.lowIndex, j = this.highIndex; i <= this.highIndex; i++, j--)
-        {
-            builder[i] = input[j];
-        }
-
-        return builder.ToString();
-    }
-}
-
-class MovePositionsInstruction : IInstruction
-{
-    private readonly int firstIndex;
-    private readonly int secondIndex;
-
-    public MovePositionsInstruction(int firstIndex, int secondIndex)
-    {
-        if (firstIndex == -1) throw new Exception();
-        if (secondIndex == -1) throw new Exception();
-
-        this.firstIndex = firstIndex;
-        this.secondIndex = secondIndex;
-    }
-
-    public string Process(string input)
-    {
-        var list = input.ToList();
-
-        var letter = input[firstIndex];
-        list.Remove(letter);
-        list.Insert(secondIndex, letter);
-
-        return String.Join("", list);
-    }
-}
