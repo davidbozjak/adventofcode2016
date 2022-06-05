0681e72cec2ce6395b34628f3da72c8eee7590ec
David Božjak <bozjak@gmail.com>
2022-06-05 21:47:57 +0200

    Added Day25

---
+++ Day25_Clock.csproj/Computer.cs
@@ -0,0 +1,104 @@
+﻿using System.Diagnostics;
+
+class Computer
+{
+    private readonly UniqueFactory<string, Register> Registers;
+    private readonly List<string> InstructionStrings;
+    private readonly Func<long, bool> handleOutputFunc;
+    private int instructionIndex;
+
+    public Computer(IEnumerable<string> instructions, Func<long, bool> handleOutputFunc)
+    {
+        this.InstructionStrings = instructions.ToList();
+        this.handleOutputFunc = handleOutputFunc;
+        this.Registers = new UniqueFactory<string, Register>(name => new Register(name));
+        this.instructionIndex = 0;
+    }
+
+    public void SetRegisterValue(string register, long value)
+    {
+        var r = Registers.GetOrCreateInstance(register);
+        r.Value = value;
+    }
+
+    public long GetRegisterValue(string register)
+    {
+        var r = Registers.GetOrCreateInstance(register);
+        return r.Value;
+    }
+
+    public void Run()
+    {
+        for (; instructionIndex >= 0 && instructionIndex < InstructionStrings.Count; instructionIndex++)
+        {
+            var instructionString = InstructionStrings[instructionIndex];
+
+            var parts = instructionString.Split(" ", StringSplitOptions.RemoveEmptyEntries);
+            var instruction = parts[0];
+
+            if (instruction == "cpy")
+            {
+                var r = Registers.GetOrCreateInstance(parts[2]);
+
+                var value = GetValueOrRegisterValue(parts[1]);
+
+                r.Value = value;
+            }
+            else if (instruction == "inc")
+            {
+                var r = Registers.GetOrCreateInstance(parts[1]);
+
+                r.Value++;
+            }
+            else if (instruction == "dec")
+            {
+                var r = Registers.GetOrCreateInstance(parts[1]);
+
+                r.Value--;
+            }
+            else if (instruction == "jnz")
+            {
+                var valueX = GetValueOrRegisterValue(parts[1]);
+
+                if (valueX != 0)
+                {
+                    var valueY = GetValueOrRegisterValue(parts[2]);
+
+                    instructionIndex += (int)(valueY - 1);
+                }
+            }
+            else if (instruction == "out")
+            {
+                var value = GetValueOrRegisterValue(parts[1]);
+
+                var shouldExecutionContinue = this.handleOutputFunc(value);
+
+                if (!shouldExecutionContinue) break;
+            }
+            else throw new Exception("Unknown instruction");
+        }
+
+        long GetValueOrRegisterValue(string valueOrRegister)
+        {
+            if (!long.TryParse(valueOrRegister, out long value))
+            {
+                var rx = Registers.GetOrCreateInstance(valueOrRegister);
+                value = rx.Value;
+            }
+
+            return value;
+        }
+    }
+
+    [DebuggerDisplay("{Name}:{Value}")]
+    protected class Register
+    {
+        public string Name { get; }
+        public long Value { get; set; } = 0;
+
+        public Register(string name)
+        {
+            this.Name = name;
+        }
+    }
+}
---
+++ Day25_Clock.csproj/Program.cs
@@ -0,0 +1,50 @@
+﻿var instructionStrings = new InputProvider<string?>("Input.txt", GetString).Cast<string>().ToList();
+
+int numberOfRequiredCorrects = 10;
+int numberOfCorrects = 0;
+long expectedValue = 0;
+
+for (long a = 1; ; a++, expectedValue = 0, numberOfCorrects = 0)
+{
+    var computer = new Computer(instructionStrings, GetOutputAndContinue);
+    computer.SetRegisterValue("a", a);
+
+    computer.Run();
+
+    if (numberOfCorrects == numberOfRequiredCorrects)
+    {
+        Console.WriteLine($"Part 1: {a}");
+        break;
+    }
+}
+
+bool GetOutputAndContinue(long value)
+{
+    if (value == expectedValue)
+    {
+        expectedValue = (expectedValue + 1) % 2;
+        numberOfCorrects++;
+
+        if (numberOfCorrects == numberOfRequiredCorrects)
+        {
+            return false;
+        }
+
+        return true;
+    }
+    else
+    {
+        return false;
+    }
+}
+
+static bool GetString(string? input, out string? value)
+{
+    value = null;
+
+    if (input == null) return false;
+
+    value = input ?? string.Empty;
+
+    return true;
+}
